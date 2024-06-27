66381ea5fd460822f48acf54cb4ba7ffd732fb65
Marta Tabor <leeloothefifth@googlemail.com>
2024-06-27 20:21:55 +0100

    connection setup

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const uriDB = process.env.DB_URL;
+const PORT = process.env.MAIN_PORT;
+
+const connection = mongoose.connect(uriDB);
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) =>
+    console.log(`Server not running. Error message: ${err.message}`)
+  );
