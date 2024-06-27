6efc4b38c5dd672e3f92be196cbf70469da48bea
Marta Tabor <leeloothefifth@googlemail.com>
2024-06-27 20:28:40 +0100

    error 400 handler add

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger));
+// app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
@@ -19,7 +19,11 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  if (err.name === "ValidationError") {
+    res.status(400).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: err.message || "Something went wrong" });
+  }
 });
 
 module.exports = app;
