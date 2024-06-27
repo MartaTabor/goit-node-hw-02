f346f4e53e07301c54bfb3a41d42b37eeb22f1d7
Marta Tabor <leeloothefifth@googlemail.com>
2024-06-27 20:51:32 +0100

    mongoose schema add

---
+++ service/schemas/contact.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const contact = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name Field is required"],
+    minLength: 2,
+    maxLength: 25,
+  },
+  email: {
+    type: String,
+    required: [true, "E-mail Field is required"],
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: "Please enter a valid email",
+    },
+  },
+  phone: {
+    type: String,
+    required: [true, "Phone Field is required"],
+  },
+});
+
+const Contact = mongoose.model("contact", contact);
+
+module.export = Contact;
