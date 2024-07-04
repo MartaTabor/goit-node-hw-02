2a937e022988f561c618d18ef80c2dd8408d3bdb
Marta Tabor <leeloothefifth@googlemail.com>
2024-07-04 20:08:00 +0100

    new libraries add and user schema creation

---
+++ service/schemas/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bCrypt = require("bcryptjs");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+userSchema.methods.setPassword = (password) => {
+  this.password = bCrypt.hashSync(password, bCrypt.genSaltSync(6));
+};
+
+userSchema.methods.validPassword = (password) => {
+  return bCrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
