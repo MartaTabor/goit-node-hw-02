6c72bd64e849703c54d385ca21fae5cc63980b25
Marta Tabor <leeloothefifth@googlemail.com>
2024-07-04 21:55:40 +0100

    auth api add

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const router = express.Router();
+const { register, login, logout } = require("../../controllers/auth");
+const { validateBody } = require("../../validation/validation");
+const auth = require("../../middlewares/jwtMiddleware");
+const { userSchema } = require("../../validation/userValidation");
+const passport = require("../../middlewares/passport-config");
+
+router.use(passport.initialize());
+
+router.post("/signup", validateBody(userSchema), register);
+router.post("/login", validateBody(userSchema), login);
+router.post("./logout", auth, logout);
+
+module.exports = router;
