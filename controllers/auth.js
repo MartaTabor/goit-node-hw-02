890e4fa62ca4ab51d65e46de4a42c4ba44398eb5
Marta Tabor <leeloothefifth@googlemail.com>
2024-07-13 08:48:33 +0100

    gravatar update to https

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,7 +17,7 @@ const register = async (req, res, next) => {
 
     const newUser = new User({ email });
     newUser.setPassword(password);
-    newUser.avatarURL = gravatar.url(email);
+    newUser.avatarURL = gravatar.url(email, { protocol: "https", s: "100" });
     await newUser.save();
 
     res.status(201).json({
