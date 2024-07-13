8dc7cef9a6835a2a61692c6e8548d69e9a2ac09d
Marta Tabor <leeloothefifth@googlemail.com>
2024-07-13 09:33:59 +0100

    file size option addd

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -15,6 +15,9 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
 });
 
 const upload = multer({ storage });
