03dd2035c0d8f6abe541e09d5661d5c8737891c2
Marta Tabor <leeloothefifth@googlemail.com>
2024-07-13 09:00:46 +0100

    upload avatar with multer use

---
+++ middlewares/upload.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const tmpDir = path.join(__dirname, "../tmp");
+
+if (!fs.existsSync(tmpDir)) {
+  fs.mkdirSync(tmpDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tmpDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
